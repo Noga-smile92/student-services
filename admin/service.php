9a8550f33743782c76b3c79e3dbac3b67f8c2af3
Noga-smile92 <104279570+Noga-smile92@users.noreply.github.com>
2022-08-17 00:25:38 +0200

    Update service.php

--- admin/service.php
+++ admin/service.php
@@ -1,69 +1,62 @@
 <?php
 include_once('config.php');
-
-class service 
+interface Service 
 {
-    public $name;
-    public $price;
-    public $image;
-    public $image_name;
-    public $image_up;
-    public $image_location;
-
- public function __construct()
-{
- $db=new Nagla;
- $this->conn= $db->conn;
- }
-
-public function upload($name,$price,$image_up)
-{
-  
-  $insert=("INSERT INTO  products (name, price, image) VALUES ('$name', '$price', '$image_up')");
-  $result=$this->conn->query($insert);
- return $result;
+   
+  public function connect();
 
 }
-public function condition($image_location,$image_up)
+class SQlconnection implements Service
 {
-    
-    move_uploaded_file($_SESSION ["image_location"],'images/'.$_SESSION["image_name"]);
-    
+   
+  public function connect()
+  {
+   
+  $db= new Nagla;
+  $this->conn=$db->conn;
+return $this->conn;
   }
-  public function updatecondition($id,$name,$price,$image_up)
+}
+
+class uploo 
+  {
+    public $uploos;
+    public function __construct(Service $uploos)
     {
-      if(isset($_SESSION["image_location"]))
-      if($_SESSION["image_location"] != "" )
-      
-{
-    $this->condition($image_location,$image_up);
-   $rr= $this->conn->query($this->update($_SESSION['id'],$name,$price,$image_up));
+      $this->uploos=$uploos;
     
-}else
-{
+     }
+     public function connect()
+     {
+      return $this->uploos->connect();
+      
+     }
+        
+  public function upload($name,$price,$image_up)
+  {
+    
+    $insert=("INSERT INTO  products (name, price, image) VALUES ('$name', '$price', '$image_up')");
+    $result=$this->conn->query($insert);
+    return $result;
+   
   
-  $rr =$this->conn->query ("UPDATE products SET name='$name', price='$price' WHERE id='{$_SESSION['id']}'");
+  }
+  
   
 }
 
-  
-  }
-               
-    
-    public function update($id,$name,$price,$image_up)
-    {
-      
-      $update = $this->conn->query("UPDATE products SET name='$name', price='$price', image='$image_up' WHERE id='{$_SESSION['id']}'");
-    
-    }
-    public function delete($id)
-    {
-      
-     $id =$this->conn->query("DELETE FROM products WHERE id= $id");
-     return $id;
-    }
 
-  }
+  
+   
+
+ 
+
+
+
+
+    
+
+  
 
 
 ?>
